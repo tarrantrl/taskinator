2c6a845d24d36d79654502921d874ffbd1270241
forsythrl <tarrantrl@utexas.edu>
2020-08-21 21:30:36 -0500

    fix js to add task on form submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+    // console.log(event);
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.textContent="New task";
     listItemEl.className="task-item";
     tasksToDoEl.appendChild(listItemEl);
   }
 
-  buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
