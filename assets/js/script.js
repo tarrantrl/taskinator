a86fdbb24cdbc81d5706c763f409b90800fb0b31
forsythrl <tarrantrl@utexas.edu>
2020-08-21 21:41:49 -0500

    get form data and use in added task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
+// console.dir(formEl);
 
 var createTaskHandler = function() {
-    // console.log(event);
     event.preventDefault();
+    // console.log(event);
+    // get form input
+    var taskNameInput = document.querySelector("input[name='task-name'").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // create list item
     var listItemEl = document.createElement("li");
-    listItemEl.textContent="New task";
     listItemEl.className="task-item";
+    // create div
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML="<h3 class='task-name'>"+taskNameInput+"</h3><span class='task-type'>"+taskTypeInput+"</span>";
+    // add info to list item
+    listItemEl.appendChild(taskInfoEl);
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
   }
 
